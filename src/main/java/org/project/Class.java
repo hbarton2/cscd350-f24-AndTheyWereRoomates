82d610db7a29ce638fc001eb867ddfc41e7f93af
Ulises <54950806+ulises-aguilar@users.noreply.github.com>
2024-10-14 17:30:56 -0700

    Update Class.java
    Added the fields for the Class: "Class".

--- src/main/java/org/project/Class.java
+++ src/main/java/org/project/Class.java
@@ -1,4 +1,10 @@
 package org.project;
 
+import java.util.ArrayList;
+
 public class Class {
+   private String name;
+   private ArrayList<Method> methodlist;
+   private ArrayList<Fields> fields;
+   private ArrayList<Relationship> relation;
 }
