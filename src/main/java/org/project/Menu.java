26ee56eb22cae48f6c84be26799806583cb252fa
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-10-15 23:44:19 -0700

    refactoring: Help command

--- src/main/java/org/project/Menu.java
+++ src/main/java/org/project/Menu.java
@@ -15,23 +15,82 @@ public class Menu {
             if(!inputCheck(userInput)) {
                 continue;
             }
-
-            System.out.println("Your command was: " + userInput);
-
         }while(!userInput.equals("exit"));
+    }
 
+    private static boolean inputCheck(String input) {
+
+        // check for empty str
+        if(input.isEmpty()) {
+            System.out.println("Please enter a command");
+            return false;
+        }
+
+        // check for number of arguments
+        String[] str = input.split(" ");
+
+        if(str[0].equals("help") && str.length < 2) {
+            return help();
+        }
+
+        return switch (str[0]) {
+            case "help" -> helpCommand(str[1]);
+            case "exit" -> false;
+            default -> true;
+        };
 
 
     }
 
-    private static boolean inputCheck(String input) {
-        return switch (input.toLowerCase()) {
-            case "add" -> true;
-            case "edit" -> true;
-            case "delete" -> true;
-            case "help" -> true;
-            case "exit" -> false;
-            default -> false;
-        };
+    private static boolean helpCommand(String command) {
+
+        switch(command) {
+            case "add":
+                System.out.println("Add command allows you to create a new class, method, field, relationship, or parameter.");
+                System.out.println("Syntax: add [object]");
+                return true;
+            case "delete":
+                System.out.println("Delete command allows you to delete existing class, method, field, relationship");
+                System.out.println("Syntax: delete [object]");
+                return true;
+            case "rename":
+                System.out.println("Rename command allows you to rename existing class, method, field");
+                System.out.println("Syntax: rename [object]");
+                return true;
+            case "save":
+                System.out.println("Save command allows you to save existing class");
+                System.out.println("Syntax: save [object]");
+                return true;
+            case "load":
+                System.out.println("Load command allows you to load existing class");
+                System.out.println("Syntax: load [object]");
+                return true;
+            case "list":
+                System.out.println("List command allows you to list existing class");
+                System.out.println("Syntax: list [object]");
+                return true;
+            case "exit":
+                System.out.println("Exits the program");
+                return false;
+            default:
+                return false;
+        }
+    }
+
+    private static boolean help() {
+        System.out.println("Syntax: help [command]");
+        System.out.println();
+        System.out.println("Existing commands are: ");
+        System.out.println("- add:  create a new class, method, field, relationship, or parameter.");
+        System.out.println("- delete: delete existing class, method, field, relationship");
+        System.out.println("- rename: rename existing class, method, field");
+        System.out.println("- save: save existing class");
+        System.out.println("- load: load existing class");
+        System.out.println("- list class: list existing class");
+        System.out.println("- list classes: list existing classes");
+        System.out.println("- list relationships: list existing relationships");
+        System.out.println("- help: shows help message");
+        System.out.println("- exit: exit application");
+        return false;
     }
 }
