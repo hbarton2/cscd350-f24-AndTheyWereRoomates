ab5bded69543db07fc0433ce52be60deb2ce5ffe
Ulises <54950806+ulises-aguilar@users.noreply.github.com>
2024-10-15 20:20:03 -0700

    Add methods/ getters/setter for Class: Class
    * Added (String name) to the setName setter in the Methods Class
    * Add methods: addMethod, removeMethod, renameMethod, removeRelation
    * Added getters/setters:
    getName(), setName(String name)

--- src/main/java/org/project/Class.java
+++ src/main/java/org/project/Class.java
@@ -1,10 +1,84 @@
 package org.project;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Class {
    private String name;
    private ArrayList<Method> methodlist;
-   private ArrayList<Fields> fields;
+   private ArrayList<Field> fields;
    private ArrayList<Relationship> relation;
+
+   public Class(String name) {
+      this.name = name;
+      this.methodlist = new ArrayList<>();
+      this.fields = new ArrayList<>();
+      this.relation = new ArrayList<>();
+   }
+
+   public boolean addMethod(String methodName) {
+      if (methodName == null || methodName.isEmpty()) {
+         throw new IllegalArgumentException("Invalid method name, try again");
+      }
+       return methodlist.add(new Method(methodName));
+   }
+
+   public boolean removeMethod(String methodName) {
+      if (methodName == null || methodName.isEmpty()) {
+         throw new IllegalArgumentException("Invalid method name, try again");
+      }
+      Iterator<Method> iterator = methodlist.iterator();
+      while (iterator.hasNext()) {
+         Method method = iterator.next();
+         if (method.getName().equals(methodName)) {
+            iterator.remove();
+            return true;
+         }
+      }
+      return false;
+   }
+
+   public boolean renameMethod(String oldName, String newName) {
+      if (oldName == null || oldName.isEmpty() || newName == null || newName.isEmpty()) {
+         throw new IllegalArgumentException("Invalid oldName or newName, try again");
+      }
+      for (Method method: methodlist) {
+         if (method.getName().equals(newName)) {
+            return false;
+         }
+      }
+      for (Method method: methodlist) {
+         if (method.getName().equals(oldName)) {
+            method.setName(newName);
+            return true;
+         }
+      }
+      return false;
+   }
+
+   public boolean removeRelation(String sourceName, String dest) {
+      if (sourceName == null || sourceName.isEmpty() || dest == null || dest.isEmpty()) {
+         throw new IllegalArgumentException("Invalid sourceName or destName, try again");
+      }
+      Iterator<Relationship> iterator = relation.iterator();
+      while (iterator.hasNext()) {
+         Relationship relationship = iterator.next();
+         if (relationship.getSource().equals(sourceName) && relationship.getDestination().equals(dest)) {
+            iterator.remove();
+            return true;
+         }
+      }
+      return false;
+   }
+
+
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
 }
+
