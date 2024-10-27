c60383aaf6d8eb676119e0ad63543ec5ae1fb59e
Rudy Madrigal <madrigalrudy34@gmail.com>
2024-10-26 17:49:08 -0700

    Set up java Scene Builder. For faster gui development

--- src/main/java/org/project/GUIView.java
+++ src/main/java/org/project/GUIView.java
@@ -1,13 +1,9 @@
 package org.project;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.util.Map;
@@ -17,40 +13,18 @@ public class GUIView extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("UML Editor");
-
-        MenuBar  menuBar = new MenuBar();
-
-
-        javafx.scene.control.Menu fileMenu = new javafx.scene.control.Menu("FILE");
-        MenuItem openFile = new MenuItem("OPEN");
-        MenuItem saveFile = new MenuItem("SAVE");
-
-        fileMenu.getItems().addAll(openFile, saveFile);
-
-
-        javafx.scene.control.Menu helpMenu = new javafx.scene.control.Menu("HELP");
-        MenuItem tutorial = new MenuItem("TUTORIAl");
-
-        helpMenu.getItems().add(tutorial);
-
-        menuBar.getMenus().addAll(fileMenu, helpMenu);
-
-        VBox sideBar = new VBox();
 
 
 
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/yes.fxml"));
+            Scene scene = new Scene(root, 400 , 400);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
-
-
-        HBox topbar = new HBox(menuBar);
-
-        BorderPane mainlayout = new BorderPane();
-        mainlayout.setTop(topbar);
-
-        Scene scene = new Scene(mainlayout, 800,800);
-        stage.setScene(scene);
-        stage.show();
 
     }
 
