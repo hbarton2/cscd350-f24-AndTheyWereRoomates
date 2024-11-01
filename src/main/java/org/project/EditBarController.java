3109ade80e1f13591434e1e4cd764de876b5f3da
Rudy Madrigal <madrigalrudy34@gmail.com>
2024-10-31 20:33:20 -0700

    edit bar created

--- src/main/java/org/project/EditBarController.java
+++ src/main/java/org/project/EditBarController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.awt.event.MouseEvent;
 import java.util.Collection;
 
 public class EditBarController {
@@ -15,7 +16,7 @@ public class EditBarController {
     private TextField className;
 
     @FXML
-    private ListView fieldList;
+    private ListView<String> fieldList;
     @FXML
     private Button addFieldButton;
     @FXML
@@ -23,8 +24,31 @@ public class EditBarController {
     @FXML
     private Button deleteFieldButton;
 
-    private ObservableList<String> obFieldlist;
 
-    public void setAddFieldButton(ActionEvent event) {
+    @FXML
+    void addField(MouseEvent event) {
+        fieldList.getItems().add("new Field");
+
+    }
+
+    public void addField(javafx.scene.input.MouseEvent mouseEvent) {
+    }
+
+    public void renameField(ActionEvent actionEvent) {
+    }
+
+    public void deleteField(ActionEvent actionEvent) {
+    }
+
+    public void addMethod(ActionEvent actionEvent) {
+    }
+
+    public void renameMethod(ActionEvent actionEvent) {
+    }
+
+    public void deleteMethod(ActionEvent actionEvent) {
+    }
+
+    public void deleteRelation(ActionEvent actionEvent) {
     }
 }
