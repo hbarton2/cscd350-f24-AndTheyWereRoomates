bd1bd7a9927b2298fef3e170473043d6d803fda8
Ulises <54950806+ulises-aguilar@users.noreply.github.com>
2024-11-01 22:02:50 -0700

    Updated Version of GUI
    Here I removed the editbar.fxml decided to just add everything in main, keep it organized. Added a little inspector edit menu.

--- src/main/java/org/project/MainController.java
+++ src/main/java/org/project/MainController.java
@@ -3,46 +3,106 @@ package org.project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
 public class MainController {
 
-
     @FXML
     private Pane canvas;
 
     @FXML
-    private Button addClass;
+    private VBox inspectorPane;
 
+    @FXML
+    private Button toggleInspectorButton;
 
-    public void createClass(ActionEvent a){
+    @FXML
+    private TextField classNameInput;
+
+    @FXML
+    private TextField fieldNameInput;
+
+    @FXML
+    private ComboBox<String> dataTypeComboBox;
+
+    @FXML
+    private TextField methodNameInput;
+
+    @FXML
+    private ComboBox<String> parametersComboBox;
+
+    private VBox selectedClassBox = null;
+
+    public void createClass(ActionEvent a) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/classNode.fxml"));
-            VBox classRepresentation = loader.load(); // Assume ClassRepresentation is a VBox
+            VBox classBox = loader.load();
+            classBox.setOnMouseClicked(event -> selectClassBox(classBox));
 
-            canvas.layoutBoundsProperty().addListener((observable, oldValue, newValue) ->{
-                double centerX = newValue.getWidth()/2;
-                double centerY = newValue.getCenterY()/2;
-                classRepresentation.setLayoutX(centerX - classRepresentation.getWidth()/2);
-                classRepresentation.setLayoutY(centerY - classRepresentation.getHeight()/2);
-
+            canvas.layoutBoundsProperty().addListener((observable, oldValue, newValue) -> {
+                double centerX = newValue.getWidth() / 2;
+                double centerY = newValue.getHeight() / 2;
+                classBox.setLayoutX(centerX - classBox.getWidth() / 2);
+                classBox.setLayoutY(centerY - classBox.getHeight() / 2);
             });
-            canvas.getChildren().add(classRepresentation);
+
+            canvas.getChildren().add(classBox);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void selectClassBox(VBox classBox) {
+        if (selectedClassBox != null) {
+            selectedClassBox.setEffect(null);
+        }
 
+        selectedClassBox = classBox;
 
+        DropShadow highlight = new DropShadow();
+        highlight.setColor(Color.BLUE);
+        highlight.setWidth(20);
+        highlight.setHeight(20);
+        selectedClassBox.setEffect(highlight);
 
+        if (selectedClassBox.getChildren().get(0) instanceof TextField) {
+            TextField classNameField = (TextField) selectedClassBox.getChildren().get(0);
+            classNameInput.setText(classNameField.getText());
+        }
+    }
 
+    @FXML
+    public void toggleInspector(ActionEvent event) {
+        boolean isInspectorVisible = inspectorPane.isVisible();
+        inspectorPane.setVisible(!isInspectorVisible);
+        toggleInspectorButton.setText(isInspectorVisible ? "Show" : "Hide");
+    }
 
+    @FXML
+    public void handleSaveClassName(ActionEvent event) {
+        if (selectedClassBox != null && selectedClassBox.getChildren().get(0) instanceof TextField) {
+            String className = classNameInput.getText();
+            TextField classNameField = (TextField) selectedClassBox.getChildren().get(0);
+            classNameField.setText(className);
+        }
+    }
+
+    @FXML
+    public void handleAddField(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void handleAddMethod(ActionEvent event) {
+
+    }
 }
