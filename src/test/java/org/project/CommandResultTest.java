c2b3f8d00797a9b20fa795d33f1b4de8db620510
Rudy Madrigal <madrigalrudy34@gmail.com>
2024-11-17 14:50:50 -0800

    Outline of what needs to be tested

---
+++ src/test/java/org/project/CommandResultTest.java
@@ -0,0 +1,19 @@
+package org.project;
+
+public class CommandResultTest {
+    /*
+    TODO: Test for the following:
+    Constructor:
+         Ensure the constructors are initialized correctly.
+    Methods:
+        isSuccess - returns the right boolean representing if the command was executed or not.
+        getMessage - returns the correct message
+        getCommandName - returns the name of the command *This is never used*
+        toString - Prints out the command result correctly
+        Testing Static Methods
+            success - Returns the true boolean as well as the message
+            failure - Returns the false boolean as as the message
+     */
+
+
+}
